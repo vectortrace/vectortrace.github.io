49caceac7a846810372d0221a89385d8486a4d8d
Theo <theodore.han@bluecrewjobs.com>
2024-08-19 11:31:49 -0700

    updates

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ git checkout --orphan gh-pages
 git add -f dist
 git commit -m "Rebuild GitHub pages"
 git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages
+git switch main
--- src/components/VTrace.tsx
+++ src/components/VTrace.tsx
@@ -1,19 +1,16 @@
-import { Artboard, Button, Hero } from "react-daisyui";
+import { Button, Hero } from "react-daisyui";
 
 export default function VTrace() {
   return (
-    <Hero className="grid-cols-1">
+    <Hero className="grid-cols-[minmax(0,1fr)]">
       <Hero.Content className="text-center">
-        <div>
+        <div className="w-8/12">
           <h1 className="text-5xl font-bold">VertexTrace</h1>
-          <p className="py-6 text-wrap text-2xl">
+          <p className="py-6 text-2xl ">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <Artboard className="mx-auto" horizontal size={6}>
-            test
-          </Artboard>
           <Button className="my-16 outline">Get started</Button>
         </div>
       </Hero.Content>
