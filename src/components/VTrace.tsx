e4bf9edd9c85896fc5f0fbe2dd3e6d90cd7f21ad
Theo <theodore.han@bluecrewjobs.com>
2024-08-19 12:32:51 -0700

    video

--- src/components/VTrace.tsx
+++ src/components/VTrace.tsx
@@ -4,15 +4,20 @@ export default function VTrace() {
   return (
     <Hero className="grid-cols-[minmax(0,1fr)]">
       <Hero.Content className="text-center">
-        <div className="w-10/12">
+        <div className="w-10/12 h-dvh">
           <h1 className="text-5xl font-bold">VertexTrace</h1>
           <p className="py-6 text-lg">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <div className="h-72 bg-gray-700">
-            VSCode gif/screenshot placeholder
+          <div className="w-full h-0 relative pt-[56.25%]">
+            <iframe
+              src="https://app.visla.us/embed/1274615970446495745"
+              frameBorder="0"
+              allow="fullscreen *"
+              className="absolute w-full h-full border-[none] left-0 top-0"
+            ></iframe>
           </div>
           <Button className="my-16 outline">Get started</Button>
         </div>
