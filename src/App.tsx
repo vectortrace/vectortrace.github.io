dca914f9167998e167f4f46d4c5e0247a45f8faf
Theo <theodore.han@bluecrewjobs.com>
2024-08-21 15:58:13 -0700

    text only

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,13 @@ function App() {
           <Title text="More Coding. Less Configuring." />
         </div>
         <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
-        <div className="mx-auto flex flex-col max-w-7xl mt-3">          
+        <div className="mx-auto flex flex-col max-w-7xl mt-3">
           <TwoColContainer>
             <Subtitle
               text="Cloud GPUs"
               subtext="Switch to cloud gpu in seconds. Eliminating the need to handle authorization, package installations, or DevOps hassles"
             />
-            <Video src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOcCNbVNk&#x2F;TDdqKCqF4lWzkqsCVtq52g&#x2F;watch?embed"/>
+            <Video src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOcCNbVNk&#x2F;TDdqKCqF4lWzkqsCVtq52g&#x2F;watch?embed" />
           </TwoColContainer>
           <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
           <TwoColContainer>
@@ -31,7 +31,10 @@ function App() {
               text="Track and Reproduce"
               subtext="Record and organize experiment details to ensure reproducibility and efficiency. All from your local workspace!"
             />
-            <Video firstOnLgScreen src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOcMHekgQ&#x2F;d5BKtBH9g_IXuDuwqSIjPg&#x2F;watch?embed"/>
+            <Video
+              firstOnLgScreen
+              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOcMHekgQ&#x2F;d5BKtBH9g_IXuDuwqSIjPg&#x2F;watch?embed"
+            />
           </TwoColContainer>
           <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
           <TwoColContainer>
@@ -39,27 +42,18 @@ function App() {
               text="Easy Setup"
               subtext="A centralize place to manage GPUs. Integrate with all major providers or register your own."
             />
-            <Video src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOcB4guYc&#x2F;da1sTvnaqpCdu80sDx9NBA&#x2F;watch?embed"/>
+            <Video src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGOcB4guYc&#x2F;da1sTvnaqpCdu80sDx9NBA&#x2F;watch?embed" />
           </TwoColContainer>
           <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
-          <TwoColContainer>
-            <Subtitle
-              text="Visualize and Compare"
-              subtext="Quickly compare different experiments and identify the best-performing models"
-            />
-            <Image firstOnLgScreen 
-                src="https://site.wandb.ai/nitropack_static/pfApWRihljemXzAqfjcIBKUphCWQwXRI/assets/images/optimized/rev-ccaa708/site.wandb.ai/wp-content/uploads/2023/07/63d13c62150c60b849593375_personas-train-01.jpg" />
-          </TwoColContainer>
+          <Subtitle
+            text="Visualize and Compare"
+            subtext="Quickly compare different experiments and identify the best-performing models with a user-friendly web console, offers intuitive insights, allowing for a seamless selection of top-performing models.  "
+          />
           <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400" />
-          <TwoColContainer>
-            <Subtitle
-              text="Collaborate"
-              subtext="Share experiments and insights with team members"
-            />
-            <Image
-              src="https://site.wandb.ai/nitropack_static/pfApWRihljemXzAqfjcIBKUphCWQwXRI/assets/images/optimized/rev-ccaa708/site.wandb.ai/wp-content/uploads/2023/07/63d13c61f294b97d97df7488_personas-train-02.jpg"
-            />
-          </TwoColContainer>
+          <Subtitle
+            text="Collaborate"
+            subtext="Share experiments and insights with team members. No more excel/doc sharing. See exactly what your teammate has been working on"
+          />
         </div>
       </div>
       <VFooter />
--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -1,8 +1,14 @@
-export default function Subtitle({ text, subtext }: { text: string, subtext:string }) {
+export default function Subtitle({
+  text,
+  subtext,
+}: {
+  text: string;
+  subtext: string;
+}) {
   return (
     <div className="my-auto font-serif">
       <p className="text-4xl text-[#F7BE38]">{text}</p>
-      <p className="text-xl">{subtext}</p>
+      <p className="text-xl w-[70%]">{subtext}</p>
     </div>
   );
 }
